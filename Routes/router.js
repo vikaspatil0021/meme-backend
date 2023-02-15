0dcc5fb521bcacd0ba1ac877c5a36dc351387b8c
vikas <vikaspatil212222@gmail.com>
2023-02-15 17:25:06 +0530

    model

--- Routes/router.js
+++ Routes/router.js
@@ -218,7 +218,7 @@ router.post("/editprofile", async (req, res) => {
 })
 
 router.post("/postviews",async (req,res)=>{
-    const no = req.body.views + 1
+    var no = req.body.views + 1
     await StoriesContent.updateOne({ _id: req.body.storyId }, { views:no });
     res.json("postviews updated")
 })
